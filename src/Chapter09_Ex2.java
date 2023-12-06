d5bb3acbd7da32f882c19aa56dda058c466c9d53
NoeSekigami <141192154+sekigaminoe@users.noreply.github.com>
2023-12-06 11:07:49 +0900

    Chapter09_Ex2-再帰処理練習

---
+++ src/Chapter09_Ex2.java
@@ -0,0 +1,16 @@
+
+public class Chapter09_Ex2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 976;
+		int b = 427;
+		System.out.println(a + "と" + b + "の最大公約数は、" + gcd(a, b) + "です");
+	}
+	static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+}
